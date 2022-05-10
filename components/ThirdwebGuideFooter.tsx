28d2096aa036374c22a1e6c1ceaa3aed01393756
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-05-09 20:01:09 -0400

    Update ThirdwebGuideFooter.tsx

--- components/ThirdwebGuideFooter.tsx
+++ components/ThirdwebGuideFooter.tsx
@@ -11,48 +11,7 @@ const deployUrl =
 export default function ThirdwebGuideFooter({ onLearnMore }: Props) {
   return (
     <div className={styles.footerContainer}>
-      {/* Left Side column */}
-      <div className={styles.left}>
-        <div>
-          <h4>Built with 💜 using</h4>
-        </div>
-        <div>
-          <a
-            href={"https://thirdweb.com/"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={`/logo.png`} alt="Thirdweb Logo" width={135} />
-          </a>
-        </div>
-      </div>
-
-      {/* Right Side column */}
-      <div className={styles.right}>
-        <a className={styles.secondaryButton} onClick={onLearnMore}>
-          Learn More
-        </a>
-
-        <a
-          href={githubUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.secondaryButton}
-          style={{ textDecoration: "none" }}
-        >
-          View on GitHub
-        </a>
-
-        <a
-          href={deployUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.mainButton}
-          style={{ textDecoration: "none" }}
-        >
-          Deploy Your Own
-        </a>
-      </div>
+ 
     </div>
   );
 }
